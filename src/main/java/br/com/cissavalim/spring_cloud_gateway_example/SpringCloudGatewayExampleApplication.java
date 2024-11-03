21e88c9992ab8d56ea659d941d4b8611a2608439
Cecilia Valim <cissamadalenarv@gmail.com>
2024-11-03 17:25:39 -0300

    first commit

---
+++ src/main/java/br/com/cissavalim/spring_cloud_gateway_example/SpringCloudGatewayExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.cissavalim.spring_cloud_gateway_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cissavalim/spring_cloud_gateway_example/SpringCloudGatewayExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cissavalim.spring_cloud_gateway_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
